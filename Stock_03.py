468e299106229f52cfdd5188953198b216b2bc86
smirk <smirk.cao@gmail.com>
2018-09-13 12:00:46 +0800

    add .py file

--- Stock_03.py
+++ Stock_03.py
@@ -5,9 +5,9 @@
 import datetime
 
 import numpy as np
+import pandas as pd
 from matplotlib import cm, pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator
-from matplotlib.finance import quotes_historical_yahoo_ochl
 
 from hmm import GaussianHMM
 from sklearn.preprocessing import scale
@@ -15,17 +15,16 @@ from sklearn.preprocessing import scale
 
 ###############################################################################
 # 导入Yahoo金融数据
-quotes = quotes_historical_yahoo_ochl(
-    "INTC", datetime.date(1995, 1, 1), datetime.date(2012, 1, 6))
+quotes = pd.read_csv('data/yahoofinance-INTC-19950101-20040412.csv')
 
-dates = np.array([q[0] for q in quotes], dtype=int) # 日期列
-close_v = np.array([q[2] for q in quotes]) # 收盘价
-volume = np.array([q[5] for q in quotes])[1:] # 交易数
-
+dates = quotes.index.values
+close_v = quotes[["Close"]].values.flatten()
+volume = quotes[["Volume"]].values.flatten()
 # diff：out[n] = a[n+1] - a[n] 得到价格变化
 diff = np.diff(close_v)
 dates = dates[1:]
 close_v = close_v[1:]
+volume = volume[1:]
 
 # scale归一化处理：均值为0和方差为1
 # 将价格和交易数组成输入数据
@@ -39,7 +38,7 @@ model.train(X)
 hidden_states = model.decode(X)
 
 # 打印参数
-print "Transition matrix: ", model.transmat_prob
+print("Transition matrix: ", model.transmat_prob)
 print("Means and vars of each hidden state")
 for i in range(model.n_state):
     print("{0}th hidden state".format(i))
